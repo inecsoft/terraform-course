cfe16352efb5be0e661660f4db87bf408f5d0b33
ivanpedrouk@gmail.com <inecsoft@inecsoft.co.uk>
2019-09-27 21:57:25 +0100

    userdata

---
+++ userdata/vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    eu-west-1 = "ami-0bbc25e23a7640b9b"
+ }
+
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
