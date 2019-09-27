46658fb988f8c82c063c077eb5d01be9688353ab
ivanpedrouk@gmail.com <inecsoft@inecsoft.co.uk>
2019-09-27 21:46:44 +0100

    demo-15-autoscaler

---
+++ demo-15-autoscaler/autoscalingpolicy.tf
@@ -0,0 +1,76 @@
+# scale up alarm
+#-----------------------------------------------------------------------------------
+resource "aws_autoscaling_policy" "example-cpu-policy-scaleup" {
+  name                   = "example-cpu-policy-scaleup"
+  autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+#--------------------------------------------------------------------------------------
+#create a alarm to triger the autoscaling policy to scale up based on cpu utilization
+#-------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "example-cpu-alarm-scaleup" {
+  alarm_name          = "example-cpu-alarm-scaleup"
+  alarm_description   = "example-cpu-alarm-scaleup"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.example-autoscaling.name
+  }
+
+  actions_enabled = true
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  alarm_actions = [aws_autoscaling_policy.example-cpu-policy-scaleup.arn]
+}
+
+#------------------------------------------------------------------------------------
+
+# scale down alarm
+resource "aws_autoscaling_policy" "example-cpu-policy-scaledown" {
+  name                   = "example-cpu-policy-scaledown"
+  autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "-1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+#---------------------------------------------------------------------------------------
+#create a alarm to triger the autoscaling policy to scale down based on cpu utilization
+#---------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "example-cpu-alarm-scaledown" {
+  alarm_name          = "example-cpu-alarm-scaledown"
+  alarm_description   = "example-cpu-alarm-scaledown"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "5"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.example-autoscaling.name
+  }
+
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.example-cpu-policy-scaledown.arn]
+}
+
+#-------------------------------------------------------------------------------------
