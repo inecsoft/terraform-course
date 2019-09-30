0998c235bc9bf8ae643ca65e64952f36cd55fee0
ivanpedrouk@gmail.com <inecsoft@inecsoft.co.uk>
2019-09-30 18:04:49 +0100

    s3-public

---
+++ s3-public/main.tf
@@ -0,0 +1,21 @@
+#------------------------------------------------------------------------------
+resource "aws_s3_bucket" "s3-bucked" {
+  bucket = "my-bucket-inecsoft"
+  #acl    = "private"
+
+  acl = "public-read"
+
+    cors_rule {
+          allowed_headers = ["*"]
+          allowed_methods = ["PUT","POST"]
+          allowed_origins = ["*"]
+          expose_headers = ["ETag"]
+          max_age_seconds = 3000
+   }
+
+      versioning {
+        enabled = true
+  }
+}
+#------------------------------------------------------------------------------
+
