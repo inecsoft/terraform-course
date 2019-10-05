d5989db48a359b3ac05bc8904fba6f8c89138eb8
ivanpedrouk@gmail.com <inecsoft@inecsoft.co.uk>
2019-10-05 19:22:18 +0100

    terra-azure

---
+++ terra-azure/.vscode/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "test" {
+  name     = "testResourceGroup1"
+  location = "West US"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_" "network" {
+  name = "network"
+  
+}
+
---
+++ terra-azure/readme.txt
@@ -0,0 +1,4 @@
+az login
+az account list
+az account set --subscription=""
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/"
