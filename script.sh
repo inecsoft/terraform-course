003841a3749eae8d858dc57a0cebdc9e60d45f73
ivanpedrouk@gmail.com <inecsoft@inecsoft.co.uk>
2019-10-05 19:25:50 +0100

    update

--- script.sh
+++ script.sh
@@ -3,16 +3,16 @@
 var=(`ls ../old`)
 
 for i in ${!var[@]}; do
-  echo $i ${var[i]}
-
+  echo $i  `du -h  ../old/${var[$i]}`
+  
   read -p "enter num: " num 
   if [ "$num" ]; then
-    cp -r ../old/${var[$num]}
+    cp -r ../old/${var[$num]} .
     git add ${var[$num]} 
     git commit -m "${var[$num]}" 
     git push
     echo "${var[$num]} was puhed to the repo"
-
+    rm -r ../old/${var[$num]}
   else
     echo "${var[$num]} will not be push to the repo"
   fi 
