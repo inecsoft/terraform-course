1dd29af3ad6897e3205d3d2268d4ac2f39a37131
ivanpedrouk@gmail.com <inecsoft@inecsoft.co.uk>
2019-10-06 13:15:42 +0100

    docker

---
+++ docker/docker-demo-2/ecs.tf
@@ -0,0 +1,50 @@
+#---------------------------------------------------------------------------------------------------------------
+#define the ECS cluster 
+#---------------------------------------------------------------------------------------------------------------
+resource "aws_ecs_cluster" "example-cluster" {
+  name = "example-cluster"
+}
+
+#---------------------------------------------------------------------------------------------------------------
+#an autoscaling group launches EC2 instances that will join this cluester
+#---------------------------------------------------------------------------------------------------------------
+resource "aws_launch_configuration" "ecs-example-launchconfig" {
+  name_prefix          = "ecs-launchconfig"
+  image_id             = var.ECS_AMIS[var.AWS_REGION]
+  instance_type        = var.ECS_INSTANCE_TYPE
+  key_name             = aws_key_pair.mykeypair.key_name
+  iam_instance_profile = aws_iam_instance_profile.ecs-ec2-role.id
+  security_groups      = [aws_security_group.ecs-securitygroup.id]
+
+  user_data            = "#!/bin/bash\necho 'ECS_CLUSTER=example-cluster' > /etc/ecs/ecs.config\nstart ecs"
+  #user_data = <<-EOF
+  #            #!/bin/bash
+  #            echo 'ECS_CLUSTER=example-cluster' > /etc/ecs/ecs.config
+  #            start ecs
+  #            EOF
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#---------------------------------------------------------------------------------------------------------------
+resource "aws_autoscaling_group" "ecs-example-autoscaling" {
+  name = "ecs-example-autoscaling"
+
+  # change the public subnets to private subnet to be more secure but you can not access these instance 
+  # directly but with a bastion
+
+  vpc_zone_identifier  = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  launch_configuration = aws_launch_configuration.ecs-example-launchconfig.name
+  min_size             = 1
+  max_size             = 1
+  tag {
+    key                 = "Name"
+    value               = "ecs-ec2-container"
+    propagate_at_launch = true
+  }
+}
+
+#---------------------------------------------------------------------------------------------------------------
---
+++ docker/docker-demo-2/readme.txt
@@ -0,0 +1,31 @@
+ 
+#get an example to build and image
+git clone https://github.com/wardviaene/docker-demo.git
+
+#cd docker-demo
+#push the an image to the ecr repo:
+docker build -t myapp-repository-url/myapp .
+docker build -t 895352585421.dkr.ecr.eu-west-1.amazonaws.com/myapp:1
+
+`aws ecr get-login`
+#if you get and error with the -e none option, just remove the argument and run it the rest
+aws ecr get-login
+
+#problem solved
+`aws ecr get-login | awk -F '-e none' '{print $1,$2}'`
+
+#push the image to the ecr repo
+docker push myapp-repository-url/myapp
+docker push 895352585421.dkr.ecr.eu-west-1.amazonaws.com/myapp:1
+
+#----------------------------------------------------------------------------------------
+#working on the cluester
+
+ssh -i mykey ec2-user@ip
+ps aux | grep agent
+docker ps
+
+
+#to troubleshut
+tail /var/log/ecs/ecs-agent.log
+
