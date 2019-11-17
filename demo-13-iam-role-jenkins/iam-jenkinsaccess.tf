87a5811a2a9c92371b35a98a3679af38b8a2d49f
inecsoft <inecsoft@inecsoft.co.uk>
2019-11-16 19:03:31 -0500

    demo-13-iam-role-jenkins

---
+++ demo-13-iam-role-jenkins/iam-jenkinsaccess.tf
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------
+resource "aws_iam_role" "jenkinsaccess" {
+    name               = "jenkinsaccess"
+    path               = "/"
+    assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+tags = {
+  Name = "jenkins" 
+  Description = "IAM Role to Use for Jenkins Integration"
+  }
+}
+#-------------------------------------------------------------
+resource "aws_iam_policy_attachment" "AWSCodePipelineCustomActionAccess-policy-attachment" {
+    name       = "AWSCodePipelineCustomActionAccess-policy-attachment"
+    policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineCustomActionAccess"
+    groups     = []
+    users      = []
+    roles      = ["jenkinsaccess"]
+}
+
+#-------------------------------------------------------------
+
