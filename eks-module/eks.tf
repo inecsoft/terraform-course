cab9952e3246a1a0f9d7bd4d5a1ea0a5bc5167b9
inecsoft <inecsoft@inecsoft.co.uk>
2019-11-18 04:59:39 -0500

    eks-module

---
+++ eks-module/eks.tf
@@ -0,0 +1,26 @@
+module "inchora-eks-cluster" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = "inchora-cluster"
+  subnets      = "${aws_subnet.eks_vpc_subnet_public.*.id}"
+  vpc_id       = "${aws_vpc.eks_vpc.id}"
+
+  #Whether to let the module manage the cluster autoscaling iam policy
+  manage_worker_autoscaling_policy = true
+  attach_worker_autoscaling_policy = yes
+
+  worker_groups = [
+    {
+      instance_type = "t2.micro"
+      asg_max_size  = 5
+      tags = [{
+        key                 = "eks"
+        value               = "worker"
+        propagate_at_launch = true
+      }]
+    }
+  ]
+
+  tags = {
+    environment = "eks"
+  }
+}
---
+++ eks-module/subnets.tf
@@ -0,0 +1,31 @@
+#-----------------------------------------------------
+#creates 3 public subnets one per availability zone
+#----------------------------------------------------
+resource "aws_subnet" "eks_vpc_subnet_public" {
+  count = "${length(var.subnet_cidr_public)}"
+  vpc_id =  "${aws_vpc.eks_vpc.id}"
+  cidr_block = "${element(var.subnet_cidr_public, count.index)}"
+  map_public_ip_on_launch = "true"
+  availability_zone  = "${element(data.aws_availability_zones.azs.names, count.index)}"
+
+
+  tags ={
+    Name = "eks_vpc_subnet_public-${count.index+1}"
+  }
+}
+#----------------------------------------------------
+#creates 3 private subnets one per availability zone
+#---------------------------------------------------
+resource "aws_subnet" "eks_vpc_subnet_private" {
+  count = "${length(var.subnet_cidr_private)}"
+  vpc_id =  "${aws_vpc.eks_vpc.id}"
+  cidr_block = "${element(var.subnet_cidr_private, count.index)}"
+  map_public_ip_on_launch = "false"
+  availability_zone  = "${element(data.aws_availability_zones.azs.names, count.index)}"
+
+  tags ={
+    Name = "eks_vpc_subnet_private-${count.index+1}"
+  }
+}
+#---------------------------------------------------------
+
