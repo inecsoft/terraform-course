945c6f74ca24979bd83926813f41426b4cb37ce1
inecsoft <inecsoft@inecsoft.co.uk>
2020-02-16 15:52:05 -0500

    installerscripts

--- installerscripts/terraformintaller.sh
+++ installerscripts/terraformintaller.sh
@@ -1,11 +1,14 @@
-TERRAFORM_ZIP_FILE=terraform_0.12.13_linux_amd64.zip
-TERRAFORM=https://releases.hashicorp.com/terraform/0.12.13
+#!/bin/bash -xv
+
+version=0.12.20
+TERRAFORM_ZIP_FILE=terraform_${version}_linux_amd64.zip
+TERRAFORM=https://releases.hashicorp.com/terraform/$version
 TERRAFORM_BIN=terraform
  
 function install_terraform {
     if [ -z $(which $TERRAFORM_BIN) ]
        then
-           wget ${TERRAFORM}/${TERRAFORM_ZIP_FILE}
+           wget  ${TERRAFORM}/${TERRAFORM_ZIP_FILE}
            unzip ${TERRAFORM_ZIP_FILE}
            sudo mv ${TERRAFORM_BIN} /usr/local/bin/${TERRAFORM_BIN}
            rm -rf ${TERRAFORM_ZIP_FILE}
