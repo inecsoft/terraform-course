0ec715d6307031758dd2bb914fc41d5cd6256f4c
inecsoft <inecsoft@inecsoft.co.uk>
2020-03-30 07:34:43 -0400

    codepipeline-nlb

---
+++ codepipeline-nlb/nlb.tf
@@ -0,0 +1,78 @@
+#-----------------------------------------------------------------------------------------------
+resource "aws_lb" "codepipeline" {
+  name                             = "codepipeline"
+  subnets                          = module.vpc.public_subnets
+  load_balancer_type               = "network"
+  enable_cross_zone_load_balancing = true
+
+  internal           = false
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${local.default_name}-alb"
+  }
+}
+
+output "nlb-dns-name" {
+  value = aws_lb.codepipeline.dns_name
+}
+#-----------------------------------------------------------------------------------------------
+resource "aws_lb_listener" "codepipeline" {
+  load_balancer_arn = aws_lb.codepipeline.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  #ssl_policy        = 
+
+  default_action {
+    target_group_arn = aws_lb_target_group.blue.id
+    type             = "forward"
+  }
+  lifecycle {
+    ignore_changes = [
+      default_action,
+    ]
+  }
+}
+#-----------------------------------------------------------------------------------------------
+resource "aws_lb_target_group" "blue" {
+  name                 = "http-blue"
+  port                 = "3000"
+  protocol             = "TCP"
+  target_type          = "ip"
+  vpc_id               = module.vpc.vpc_id
+  deregistration_delay = "30"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    protocol            = "TCP"
+    interval            = 30
+  }
+
+  tags = {    
+    name =  "${local.default_name}-blue-tg"
+  } 
+}
+#-----------------------------------------------------------------------------------------------
+resource "aws_lb_target_group" "green" {
+  name                 = "http-green"
+  port                 = "3000"
+  protocol             = "TCP"
+  target_type          = "ip"
+  vpc_id               = module.vpc.vpc_id
+  deregistration_delay = "30"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    protocol            = "TCP"
+    interval            = 30
+  }
+   tags = {    
+    Name =  "${local.default_name}-green-tg"    
+  } 
+}
+#-----------------------------------------------------------------------------------------------
+
