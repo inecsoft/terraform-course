ddfa5f952662217bc12147a311d7980d26f84006
inecsoft <inecsoft@inecsoft.co.uk>
2020-03-31 19:25:14 -0400

    billing-alarm

---
+++ billing-alarm/cloudwatch-metric.tf
@@ -0,0 +1,29 @@
+#-----------------------------------------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "account_billing_alarm_to_new_sns" {
+    depends_on = [aws_sns_topic.sns_alert_topic]
+    alarm_name                = "${local.default_name}-alarm"
+    actions_enabled           = true
+    alarm_actions             = [
+        "${aws_sns_topic.sns_alert_topic.arn}",
+    ]
+    alarm_description         = "the threshold of $10  has been reached."
+    comparison_operator       = "GreaterThanOrEqualToThreshold"
+    datapoints_to_alarm       = 1
+    dimensions                = {
+        "Currency" = "${var.currency}"
+    }
+    evaluation_periods        = 1
+    insufficient_data_actions = []
+    metric_name               = "EstimatedCharges"
+    namespace                 = "AWS/Billing"
+    ok_actions                = []
+    period                    = 21600
+    statistic                 = "Maximum"
+    threshold                 = 10
+    treat_missing_data        = "missing"
+
+    tags                      = {
+       Name =  "${local.default_name}-alarm"
+    }
+}
+#-------------------------------------------------------------------------------------------------------------------------
