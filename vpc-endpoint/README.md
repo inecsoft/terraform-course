acb513bafdab010f8db1a01321e18b2c22a99a45
inecsoft <inecsoft@inecsoft.co.uk>
2020-04-01 18:03:19 -0400

    vpc-endpoint

---
+++ vpc-endpoint/README.md
@@ -0,0 +1,21 @@
+***
+
+<div aling="center">VPC Endpoint</div>
+
+***
+#### __*NOTE:*__  
+   VPC with enabled VPC flow log to S3 and CloudWatch logs.  
+   Cloudwatch log group and IAM role will be created within the new vpc.  
+
+### __VPC Endpoints Supports:__
+  * ####  _Gateway_:  
+    S3, DynamoDB  
+  * #### _Interface:_  
+    EC2, SSM, EC2 Messages, SSM Messages, SQS, ECR API, ECR DKR, API Gateway, KMS, ECS, ECS Agent, ECS Telemetry, SNS, STS, Glue, CloudWatch(Monitoring, Logs, Events), Elastic Load Balancing, CloudTrail, Secrets Manager, Config, CodeBuild, CodeCommit, Git-Codecommit, Transfer Server, Kinesis Streams, Kinesis Firehose, SageMaker(Notebook, Runtime, API), CloudFormation, CodePipeline, Storage Gateway, AppMesh, Transfer, Service Catalog, AppStream, Athena, Rekognition, Elastic File System (EFS), Cloud Directory
+
+
+***
+
+#### __To test:__
+Delete the route table for the natgateway and create the endpoint then test that the EC2 instance has access to S3 bucket.
+check that you have a route table pointing to the endpoint
---
+++ vpc-endpoint/vpc.tf
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.24.0"
+
+  name = "${local.default_name}"
+  cidr = "10.0.0.0/16"
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+
+  enable_dhcp_options               = true
+  dhcp_options_domain_name          = "${local.default_name}"
+  #dhcp_options_domain_name_servers = ["127.0.0.1", "10.0.1.100"]
+
+  # VPC endpoint for S3 (successful)
+ # enable_s3_endpoint = true
+
+  #VPC Flow Logs (Cloudwatch log group and IAM role will be created) needs fixing
+#  enable_flow_log                      = true
+#  create_flow_log_cloudwatch_log_group = true
+#  create_flow_log_cloudwatch_iam_role  = true
+
+  tags = {
+    "Name" = "${local.default_name}"
+  }
+}
+
+#-------------------------------------------------------------------
