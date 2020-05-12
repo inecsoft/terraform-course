a46be7e9e5e7b44a84a1f3f27de6dbd2f97c7a39
inecsoft <inecsoft@inecsoft.co.uk>
2020-05-12 15:57:14 -0400

    zappa-django

---
+++ zappa-django/iam.tf
@@ -0,0 +1,30 @@
+##################################################
+# Create an IAM role to allow enhanced monitoring
+##################################################
+#----------------------------------------------------------------------------
+resource "aws_iam_role" "rds_enhanced_monitoring_db" {
+  name_prefix        = "rds-enhanced-monitoring-db-"
+  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
+}
+#----------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring_db" {
+  role       = aws_iam_role.rds_enhanced_monitoring_db.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+#----------------------------------------------------------------------------
+data "aws_iam_policy_document" "rds_enhanced_monitoring" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+  }
+}
+#----------------------------------------------------------------------------
+
---
+++ zappa-django/instance.tf
@@ -0,0 +1,45 @@
+
+#-----------------------------------------------------------------------------------------------
+#create a template to store the set of instruction to install on the instance to access the RDS
+#-----------------------------------------------------------------------------------------------
+
+data "template_file" "userdata" {
+   template = file("userdata.tpl")
+
+}
+
+#----------------------------------------------------------------------------
+#create an instance to access the RDS
+#----------------------------------------------------------------------------
+resource "aws_instance" "manage" {
+#  ami           = var.AMIS[var.AWS_REGION]
+  ami                          = data.aws_ami.redhat.id
+  instance_type                = "t2.micro"
+
+  # the VPC subnet
+  subnet_id                    = element(module.vpc.public_subnets,0)
+
+  # the security group
+  vpc_security_group_ids       = [aws_security_group.manage-instance.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+ 
+  #use userdata to install mysql-client on the instance to access the RDS server
+   user_data = data.template_file.userdata.rendered
+ 
+   associate_public_ip_address = true
+   root_block_device {
+     volume_type = "gp2"
+     volume_size = "8"
+     delete_on_termination = true
+
+   }
+
+  tags = {
+    Name = "${local.default_name}-rds-management"
+   }
+}
+#----------------------------------------------------------------------------
+
+
