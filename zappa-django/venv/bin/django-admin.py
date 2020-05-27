35d1e3bc258336d5752b0b13a09f0ce3210b0d5d
inecsoft <inecsoft@inecsoft.co.uk>
2020-05-27 18:19:22 -0400

    zappa-django

---
+++ zappa-django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/devop/terraform/tutorial/zappa-django/Currency_Rate/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
