1ba2702c830db9e462197bd24f304a1e1a9af1c0
inecsoft <inecsoft@inecsoft.co.uk>
2020-05-28 17:55:44 -0400

    github

---
+++ github/gitrepo.tf
@@ -0,0 +1,6 @@
+#-------------------------------------------------------------------$
+resource "github_repository" "git-devops" {
+  name        = "git-devops"
+  description = "Repository for public terraform code using in Otus DevOps course"
+}
+#-------------------------------------------------------------------$
