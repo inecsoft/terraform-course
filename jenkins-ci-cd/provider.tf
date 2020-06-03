7d8b430daa5052bcde3175334e0595acd5721db4
inecsoft <inecsoft@inecsoft.co.uk>
2020-06-03 16:17:58 -0400

    jenkins-ci-cd

---
+++ jenkins-ci-cd/provider.tf
@@ -0,0 +1,15 @@
+#---------------------------------------------------------
+provider "aws" {
+  region                  = var.AWS_REGION
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = var.profile
+}
+#---------------------------------------------------------
+provider "github" {
+  #token        = var.github_token
+  token        = file(" ~/.ssh/webhooktoken")
+  organization = var.github_organization
+  individual    =  false
+}
+
+#---------------------------------------------------------
