b64a046ad1c930f670f6a48a9a020a8df3c8eda8
inecsoft <inecsoft@inecsoft.co.uk>
2020-07-08 18:41:50 -0400

    database need migrating

---
+++ dotnet-mvc-postgres/MvcMovie/Migrations/20200708195208_final.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
