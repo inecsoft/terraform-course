6c4cba2c112de674967657104e968baded98a38f
inecsoft <ivanpedrouk@gmail.com>
2021-05-22 16:17:05 +0100

    update the ip waf project

---
+++ acl-waf-ipgroup/10-waf-api-gw.tf
@@ -0,0 +1,89 @@
+#-----------------------------------------------------------------------------
+# resource "aws_wafv2_web_acl_association" "wafv2-web-acl-association" {
+#   resource_arn = aws_api_gateway_stage.api-gateway-stage-dev.arn
+#   web_acl_arn  = aws_wafv2_web_acl.wafv2-web-acl.arn
+# }
+#-----------------------------------------------------------------------------
+#terraform import aws_wafv2_web_acl.wafv2-web-acl 4250059d-abb3-4e86-b073-8dd60bebdc0d/name-dev-web-acl-tfgm/REGIONAL
+#-----------------------------------------------------------------------------
+resource "aws_wafv2_web_acl" "wafv2-web-acl" {
+  name        = "${local.default_name}-managed-rule-wafv2-web-acl-ipgroup"
+  description = "${local.default_name}-managed-rule-wafv2-web-acl-ipgroup to protect api"
+  scope       = "REGIONAL"
+
+
+  default_action {
+    # allow {}
+    block {}
+  }
+
+  rule {
+    name     = "${local.default_name}-managed-rule-wafv2-web-acl-ipgroup"
+    priority = 0
+
+    action {
+      allow {}
+    }
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.wafv2-ip-set.arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.default_name}-managed-rule-wafv2-web-acl-ipgroup"
+      sampled_requests_enabled   = true
+    }
+  }
+  
+  rule {
+    name     = "${local.default_name}-rule-wafv2-web-acl-ipgroup-GB"
+    priority = 1
+
+    action {
+      allow {}
+    }
+
+    statement {
+      geo_match_statement {
+        country_codes = ["GB"]
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.default_name}-rule-wafv2-web-acl-ipgroup-GB"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${local.default_name}-managed-rule-wafv2-web-acl"
+    sampled_requests_enabled   = true
+  }
+
+
+  tags = {
+    Name  = "${local.default_name}-managed-rule-wafv2-web-acl"
+    Environment = terraform.workspace
+  }
+
+}
+#-----------------------------------------------------------------------------
+resource "aws_wafv2_ip_set" "wafv2-ip-set" {
+  name               = "${local.default_name}-wafv2IpSet-for-wafv2-web-acl"
+  description        = "${local.default_name}-wafv2IpSet-for-wafv2-web-acl allow web access"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+  addresses          = ["1.2.3.4/32", "5.6.7.8/32"]
+
+  tags = {
+    Name = "${local.default_name}-wafv2IpSet-for-wafv2-web-acl"
+    Environment = terraform.workspace
+  }
+}
+
+#-----------------------------------------------------------------------------
---
+++ acl-waf-ipgroup/2-vars.tf
@@ -0,0 +1,70 @@
+#--------------------------------------------------------------------------------------
+data "http" "workstation-external-ip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+locals {
+  workstation-external-cidr = "${chomp(data.http.workstation-external-ip.body)}/32"
+}
+#----------------------------------------------------------------------------
+variable "AWS_REGION" {
+  default = "eu-west-2"
+}
+#----------------------------------------------------------------------------------------
+#In order to allow easy switching between versions you can define 
+#a variable to allow the version number to be chosen dynamically
+#----------------------------------------------------------------------------------------
+variable "app_versions" {
+  default = "20200923163119"
+}
+#----------------------------------------------------------------------------------------
+variable "FUNCTION_NAME" {
+  default = "serverlesfunction"
+}
+#-------------------------------------------------------------------
+locals {
+  default_name = join("-", list(terraform.workspace, "dba"))
+}
+#-------------------------------------------------------------------
+resource "random_pet" "this" {
+  length = 2
+}
+#-------------------------------------------------------------------
+resource "random_password" "master" {
+  length = 10
+}
+#-------------------------------------------------------------------
+data "aws_availability_zones" "azs" {}
+#-------------------------------------------------------------------
+data "aws_iam_account_alias" "current" {}
+#-------------------------------------------------------------------
+#---------------------------------------------------------
+# The map here can come from other supported configurations
+# like locals, resource attribute, map() built-in, etc.
+#---------------------------------------------------------
+locals {
+  app_version = formatdate("YYYYMMDDHHmmss", timestamp())
+}
+#---------------------------------------------------------
+variable "phone_number" {
+  type = string
+  default = "+447518527690"
+} 
+#---------------------------------------------------------
+variable "api_name" {
+  type = string
+  default = "ACME-Shoes-rest-api"
+} 
+#---------------------------------------------------------
+variable "enable_alarms" {
+  type = string
+  default = "1"
+} 
+#---------------------------------------------------------
+data "aws_caller_identity" "current" {}
+#---------------------------------------------------------
+variable "stage_name" {
+  type = string
+  default = "dev"
+} 
+#---------------------------------------------------------
