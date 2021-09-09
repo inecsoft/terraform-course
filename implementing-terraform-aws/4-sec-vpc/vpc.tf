bef69c7059968a25d8426dc1742c01164f570e37
inecsoft <ivanpedrouk@gmail.com>
2021-09-09 21:07:17 +0100

    vpc for peering conectivity

---
+++ implementing-terraform-aws/4-sec-vpc/vpc.tf
@@ -0,0 +1,28 @@
+
+
+#############################################################################
+# RESOURCES
+#############################################################################  
+
+# Create security VPC
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  # version = "2.33.0"
+
+  name = "sec-vpc"
+  cidr = var.vpc_cidr_range
+
+  azs             = slice(data.aws_availability_zones.azs.names, 0, 2)
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  intra_subnets   = var.intra_subnets
+
+  tags = {
+    Environment = "all"
+    Team        = "security"
+  }
+
+}
+#############################################################################
+
