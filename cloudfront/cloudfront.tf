8427def609d5567bd4aa715468be65737d28569f
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2022-06-30 18:46:32 +0000

    update

--- cloudfront/cloudfront.tf
+++ cloudfront/cloudfront.tf
@@ -64,6 +64,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     viewer_certificate {
         cloudfront_default_certificate = true
         minimum_protocol_version       = "TLSv1"
+        #acm_certificate_arn            = 
     }
 
     tags                           = {
