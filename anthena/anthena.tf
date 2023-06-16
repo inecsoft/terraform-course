f4f1aa9deecb5899c36a5ae86d72ff117c297b98
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2023-06-16 12:37:42 +0000

    not finished project Fri Jun 16 12:37:42 UTC 2023

--- anthena/anthena.tf
+++ anthena/anthena.tf
@@ -1,5 +1,13 @@
 resource "aws_s3_bucket" "s3_bucket_anthena" {
   bucket = "ca-anthena-cloudtrail-db-tfgm"
+  force_destroy = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_kms_key" "kms_key" {
--- anthena/cloudtrail.tf
+++ anthena/cloudtrail.tf
@@ -36,6 +36,14 @@ resource "aws_s3_bucket" "s3_bucket_aws_cloudtrail_logs" {
     bucket = "s3-bucket-cloudtrail-logs-tfgm"
 	force_destroy = true
 
+	server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_aws_cloudtrail_logs" {
