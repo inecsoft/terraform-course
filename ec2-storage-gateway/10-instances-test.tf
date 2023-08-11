eaa7a97bfefd71d4306d4ad34b7fe2e43d57c62a
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2023-08-11 11:47:50 +0000

    update

--- ec2-storage-gateway/10-instances-test.tf
+++ ec2-storage-gateway/10-instances-test.tf
@@ -88,4 +88,6 @@ output "webserver-public-ip" {
   description = "webserver-public ip"
   value       = aws_instance.webserver.public_ip
 }
+
+
 #-------------------------------------------------------------------------
