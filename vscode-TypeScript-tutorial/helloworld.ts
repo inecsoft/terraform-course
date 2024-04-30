35b29094ee9f677570c51dccde960029dcaa35e4
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2024-04-30 17:04:48 +0000

    added typescript tutorial

---
+++ vscode-TypeScript-tutorial/helloworld.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello World';
+// if (false) {
+//   message = 'Hello TypeScript';
+// }
+console.log(message);
+console.warn(message);
---
+++ vscode-TypeScript-tutorial/out/helloworld.js
@@ -0,0 +1,7 @@
+var message = 'Hello World';
+// if (false) {
+//   message = 'Hello TypeScript';
+// }
+console.log(message);
+console.warn(message);
+//# sourceMappingURL=helloworld.js.map
