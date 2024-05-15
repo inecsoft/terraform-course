8fd94dbbd796f55699ff4f16a2b800812b1acef4
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2024-05-15 14:34:19 +0000

    update

--- Terraform-Map-Variable/main.tf
+++ Terraform-Map-Variable/main.tf
@@ -1,12 +1,51 @@
 
+# range(max)
+# range(start, limit)
+# range(start, limit, step)
+
+variable "name_counts" {
+  type    = map(number)
+  default = {
+    "foo" = 2
+    "bar" = 4
+  }
+}
+
+locals {
+  expanded_names = {
+    for name, count in var.name_counts : name => [
+      for i in range(count) : format("%s%03d", name, i)
+    ]
+  }
+}
+
+output "expanded_names" {
+  value = local.expanded_names
+}
+
+#convert list to map with index
+locals {
+  characters_name = ["luke", "yoda", "darth"]
+  enemies_destroyed_count = [4252, 900, 20000056894]
+
+  map_index = {
+    for index, character in toset(local.characters_name) :
+      character => local.enemies_destroyed_count[index(local.characters_name, index)]
+  }
+}
+
+output "convert_list_to_map_index" {
+  value = local.map_index
+}
+
 #convert list to map
+
 locals {
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
 
   map = {
     for character in toset(local.characters) : # Convert character list to a set
-      # character => local.enemies_destroyed[index()]
       character => local.enemies_destroyed
   }
 }
